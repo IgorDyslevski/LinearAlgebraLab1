b48e518f37ad4ac7f1062b9f545e089bcec8081d
Игорь Дышлевский <dim@Igors-MacBook-Air.local>
2024-12-08 15:22:30 +0300

    Класс Матрицы обновлен

--- matrix.py
+++ matrix.py
@@ -1,38 +1,53 @@
-class Matrix:
-    def __init__(self, matrix=None, sparse=None):
-        assert not (matrix is None and sparse is None)
-        if matrix is None:
-            self.sparse = sparse
-        else:
-            self.sparse = self.matrix2sparse(matrix)
+class DenseMatrix():
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.n = len(matrix)
+        self.m = len(matrix[0])
 
-    def matrix2sparse(self, matrix):
-        assert len(matrix) > 0 and len(matrix[0]) > 0
+    def __getitem__(self, idx):
+        return self.matrix[idx]
+
+
+class SparseMatrix():
+    def __init__(self, sparse, n, m):
+        assert len(sparse[0]) == len(sparse[1]) and len(sparse[0]) > 0 and len(sparse[2]) > 0
+        self.sparse = sparse
+        self.n = n
+        self.m = m
+
+
+class Matrix:
+    def __init__(self, matrix: DenseMatrix | SparseMatrix):
+        if type(matrix) is DenseMatrix:
+            matrix = self.dense2sparse(matrix)
+        self.matrix = matrix
+
+    def dense2sparse(self, dense: DenseMatrix) -> SparseMatrix:
+        assert dense.n > 0 and dense.m > 0
         values = []
         col_indices = []
         row_ptr = [0]
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                value = matrix[i][j]
+        for i in range(dense.n):
+            for j in range(dense.m):
+                value = dense[i][j]
                 if value != 0:
                     values.append(value)
                     col_indices.append(j)
             row_ptr.append(len(values))
-        return values, col_indices, row_ptr
+        return SparseMatrix([values, col_indices, row_ptr], dense.n, dense.m)
 
-    def sparse2matrix(self, sparse):
-        values, col_indices, row_ptr = sparse
-        assert len(values) == len(col_indices) and len(values) > 0 and len(row_ptr) > 0
-        rows = len(row_ptr) - 1
-        cols = max(col_indices) + 1
+    def sparse2dense(self, sparse: SparseMatrix) -> DenseMatrix:
+        values, col_indices, row_ptr = sparse.sparse
+        rows = sparse.n
+        cols = sparse.m
         matrix = [[0] * cols for _ in range(rows)]
-        for i in range(len(row_ptr) - 1):
+        for i in range(rows):
             for j in range(row_ptr[i], row_ptr[i + 1]):
                 matrix[i][col_indices[j]] = values[j]
-        return matrix
+        return DenseMatrix(matrix)
 
     def __str__(self):
-        return str(self.sparse)
+        return str(self.matrix.sparse)
 
     def __repr__(self):
-        return f'Matrix(sparse={str(self.sparse)})'
+        return f'Matrix(SparseMatrix({str(self.matrix.sparse)}, {self.matrix.n}, {self.matrix.m}))'
