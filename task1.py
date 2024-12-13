23df52207b450a0f98d7710da51b66e575ebdcf4
Игорь Дышлевский <dim@Igors-MacBook-Air.local>
2024-12-13 19:54:21 +0300

    +функция для разреженного вида

--- task1.py
+++ task1.py
@@ -1,6 +1,10 @@
 from matrix import *
 
-
+def dense2sparse(n, m, matrix):
+    dense = DenseMatrix(matrix)
+    sparse = MatrixUtils.dense2sparse(dense)
+    return sparse.matrix()
+    
 def trace(n, m, matrix):
     if n != m:
         print("N дожно быть равно M для подсчета следа")
@@ -15,6 +19,7 @@ def get_by_idx(n, m, matrix, i, j):
 
 if __name__ == '__main__':
     dense = MatrixUtils.read2dense('Введите N и M', 'Введите матрицу')
+    print('sparse:', dense2sparse(dense.n(), dense.m(), dense.matrix()))
     if dense.n() == dense.m():
         print('trace:', trace(dense.n(), dense.m(), dense.matrix()))
     i, j = map(int, input('Введите i и j\n').split())
